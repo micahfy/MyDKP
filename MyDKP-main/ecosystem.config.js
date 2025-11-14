fd3d096fe8785b92680383dd5b7cbb1786231595
micahfy <micahfy@hotmail.com>
2025-11-14 10:15:08 +0800

    pm2fix2

--- MyDKP-main/ecosystem.config.js
+++ MyDKP-main/ecosystem.config.js
@@ -2,18 +2,16 @@ module.exports = {
   apps: [
     {
       name: 'dkp-manager',
-      cwd: '/var/www/MyDKP/MyDKP-main',   // 改成你的实际目录
-      script: 'npm',
-      args: 'run start',                 // 生产模式执行 next start
+      cwd: '/var/www/MyDKP/MyDKP-main',
+      script: 'node_modules/.bin/next',
+      args: 'start',
       exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
       env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
         NODE_ENV: 'production',
+        PORT: 3000
       },
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       error_file: './logs/error.log',
