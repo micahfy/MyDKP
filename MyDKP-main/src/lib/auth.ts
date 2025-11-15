25b8065590dc6c21f6ad90f1ec816612fc9ceb5e
micahfy <micahfy@hotmail.com>
2025-11-15 14:45:52 +0800

    bugfix6

--- MyDKP-main/src/lib/auth.ts
+++ MyDKP-main/src/lib/auth.ts
@@ -15,7 +15,7 @@ const sessionOptions = {
   password: process.env.SESSION_SECRET!,
   cookieName: 'wow-dkp-session',
   cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.COOKIE_SECURE === 'true',
     httpOnly: true,
     sameSite: 'lax' as const,
     maxAge: 60 * 60 * 24 * 7, // 7 days
